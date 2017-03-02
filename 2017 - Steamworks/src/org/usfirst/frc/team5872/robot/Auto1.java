34a348e69f302694c8c3ec053ae2398589a8fc16
Arman Hasan <h.arman88@yahoo.com>
2017-03-02 15:41:32 -0500

    Spaced Auto1 code

--- 2017 - Steamworks/src/org/usfirst/frc/team5872/robot/Auto1.java
+++ 2017 - Steamworks/src/org/usfirst/frc/team5872/robot/Auto1.java
@@ -6,39 +6,52 @@ import org.usfirst.frc.team5872.robot.Robot;
 public class Auto1 extends CommandGroup {
 	
 	public void execute() {
+		
 		Robot.runMotor(0.5);
     	Robot.delay(1250);
+    	
     	Robot.runMotor(0.25);
     	Robot.delay(1250);
+    	
     	Robot.runMotor(0);
     	Robot.delay(1500);
+    	
     	Robot.runMotor(-0.5);
     	Robot.delay(75);
+    	
     	Robot.runMotor(0);
     	Robot.delay(50);
+    	
     	Robot.turn(0.3, -0.3);
     	Robot.delay(750);
+    	
     	Robot.runMotor(0);
     	Robot.bangBang(0.4136);
+    	
     	Robot.delay(1500);
     	Robot.runMotor(0.0);
+    	
     	Robot.bangBang(0.4136);
     	Robot.outtake.set(0.5);
     	Robot.delay(250);
+    	
     	Robot.bangBang(0.4136);
     	Robot.outtake.set(0);
     	Robot.mixer.set(-1.0);
     	Robot.delay(5000);
+    	
     	Robot.runMotor(0);
     	Robot.outtake.set(-0.5);
     	Robot.shooter.set(0);
     	Robot.mixer.set(0);
     	Robot.delay(250);
+    	
     	Robot.runMotor(0);
     	Robot.outtake.set(0);
     	Robot.shooter.set(0);
     	Robot.mixer.set(0);
     	Robot.delay(5000);
+    	
 	}
 	 /*public void delay(int milliseconds){
 	    	try{
