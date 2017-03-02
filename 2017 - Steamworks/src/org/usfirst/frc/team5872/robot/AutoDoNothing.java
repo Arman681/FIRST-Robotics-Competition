506e6107256f1a332119adc3a3d9ea7409a632a6
Arman Hasan <h.arman88@yahoo.com>
2017-03-02 15:22:14 -0500

    Initialized AutoDoNothing Class

---
+++ 2017 - Steamworks/src/org/usfirst/frc/team5872/robot/AutoDoNothing.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc.team5872.robot;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class AutoDoNothing extends CommandGroup {
+	
+	public void execute() {
+		
+	}
+
+}
