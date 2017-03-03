b4b7921b2b5c6a0789f895ca9424835370bc020c
Arman Hasan <h.arman88@yahoo.com>
2017-03-03 08:57:59 -0500

    Initialization of AutoMiddleGear auto

---
+++ 2017 - Steamworks/src/org/usfirst/frc/team5872/robot/AutoMiddleGear.java
@@ -0,0 +1,54 @@
+package org.usfirst.frc.team5872.robot;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class AutoMiddleGear {
+
+public void execute() {
+	Robot.runMotor(0.5);
+	Robot.delay(1250);
+	
+	Robot.runMotor(0.25);
+	Robot.delay(1250);
+	
+	Robot.runMotor(0);
+	Robot.delay(3000);
+	
+	Robot.runMotor(-0.5);
+	Robot.delay(75);
+	
+	Robot.runMotor(0);
+	Robot.delay(50);
+	
+	Robot.turn(0.3, -0.3);
+	Robot.delay(1250);
+	
+	Robot.runMotor(0);
+	Robot.bangBang(0.4136);
+	
+	Robot.delay(1500);
+	Robot.runMotor(0.0);
+	
+	Robot.bangBang(0.4136);
+	Robot.outtake.set(0.5);
+	Robot.delay(250);
+	
+	Robot.bangBang(0.4136);
+	Robot.outtake.set(0);
+	Robot.mixer.set(1.0);
+	Robot.delay(5000);
+	
+	Robot.runMotor(0);
+	Robot.outtake.set(-0.5);
+	Robot.shooter.set(0);
+	Robot.mixer.set(0);
+	Robot.delay(250);
+	
+	Robot.runMotor(0);
+	Robot.outtake.set(0);
+	Robot.shooter.set(0);
+	Robot.mixer.set(0);
+	Robot.delay(3750);	
+	}
+	
+}
