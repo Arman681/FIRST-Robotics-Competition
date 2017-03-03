32406a1a49e4bdfe4c153f6f79da1d5497bab5bd
Arman Hasan <h.arman88@yahoo.com>
2017-03-03 09:29:19 -0500

    Extended CommandGroup

--- 2017 - Steamworks/src/org/usfirst/frc/team5872/robot/AutoMiddleGear.java
+++ 2017 - Steamworks/src/org/usfirst/frc/team5872/robot/AutoMiddleGear.java
@@ -3,7 +3,7 @@ package org.usfirst.frc.team5872.robot;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import org.usfirst.frc.team5872.robot.Robot;
 
-public class AutoMiddleGear {
+public class AutoMiddleGear extends CommandGroup {
 
 public void execute() {
 	Robot.runMotor(0.5);
--- 2017 - Steamworks/src/org/usfirst/frc/team5872/robot/AutoWhiteLine.java
+++ 2017 - Steamworks/src/org/usfirst/frc/team5872/robot/AutoWhiteLine.java
@@ -3,7 +3,7 @@ package org.usfirst.frc.team5872.robot;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import org.usfirst.frc.team5872.robot.Robot;
 
-public class AutoWhiteLine {
+public class AutoWhiteLine extends CommandGroup {
 
 	public void execute() {
 		
