d8829562cfb4c3437ab20a10c548fba73d256468
Arman Hasan <h.arman88@yahoo.com>
2017-03-10 08:38:31 -0500

    Removed gear_pivot calls

--- 2017 - Steamworks/src/org/usfirst/frc/team5872/robot/Auto1.java
+++ 2017 - Steamworks/src/org/usfirst/frc/team5872/robot/Auto1.java
@@ -25,49 +25,21 @@ public class Auto1 extends CommandGroup {
     	
     	Robot.stopDriveTrain();
     	Robot.bangBang(0.4136);
-    	Robot.outtake.set(0.5);
     	Robot.delay(250);
     	
     	Robot.bangBang(0.4136);
-    	Robot.outtake.set(0);
     	Robot.mixer.set(1.0);
     	Robot.delay(5000);
     	
     	Robot.stopDriveTrain();
-    	Robot.outtake.set(-0.5);
     	Robot.shooter.set(0);
     	Robot.mixer.set(0);
     	Robot.delay(250);
     	
     	Robot.stopDriveTrain();
-    	Robot.outtake.set(0);
     	Robot.shooter.set(0);
     	Robot.mixer.set(0);
     	Robot.delay(3750);
     	
 	}
-	 /*public void delay(int milliseconds){
-	    	try{
-	    		Thread.sleep(milliseconds);
-	    	}
-	    	catch(Exception e1){
-	    		e1.printStackTrace();
-	    	}
-	    }
-	    public void turn(double left,double right)
-	    {
-	    	fl.set(left);
-	    	fr.set(right);
-	    	bl.set(left);
-	    	br.set(right);
-	    }	
-	    
-	    public static void runMotor(double speed)
-	    {
-	    	fl.set(speed*.945);
-	    	fr.set(speed);
-	    	bl.set(speed*.945);
-	    	br.set(speed);
-	    }*/
-
 }
